78304cdc3f9575e6b220309a714cfc41363ce7e2
Hypershad1c <spox618@gmail.com>
2025-12-18 08:51:35 +0100

    Fix Gradle Kotlin DSL + regenerate plugins

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,3 @@
-// android/app/build.gradle
-// Update these sections in your build.gradle file
-
 plugins {
     id "com.android.application"
     id "kotlin-android"
@@ -8,52 +5,45 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_todo_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    namespace "com.example.flutter_todo_app"
+    compileSdk flutter.compileSdkVersion
+    ndkVersion flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (com.yourcompany.appname).
-        applicationId = "com.example.flutter_todo_app"
-        
-        // IMPORTANT: Set minSdk to at least 21 for sqflite
-        minSdk = 21
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-        
-        // Enable multidex support
-        multiDexEnabled = true
+        applicationId "com.example.flutter_todo_app"
+
+        // REQUIRED for sqflite
+        minSdk 21
+        targetSdk flutter.targetSdkVersion
+
+        versionCode flutter.versionCode
+        versionName flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.debug
-            
-            // Optimize APK size
-            minifyEnabled = true
-            shrinkResources = true
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            // Temporary signing (OK for APK testing)
+            signingConfig signingConfigs.debug
+
+            // APK optimization
+            minifyEnabled true
+            shrinkResources true
+            proguardFiles getDefaultProguardFile(
+                    'proguard-android-optimize.txt'
+            ), 'proguard-rules.pro'
         }
     }
 }
 
 flutter {
-    source = "../.."
-}
-
-dependencies {
-    // Add if you encounter multidex issues
-    implementation 'androidx.multidex:multidex:2.0.1'
+    source "../.."
 }
